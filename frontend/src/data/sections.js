4da702cbac9a5c03d9c57ae5ade4038e29bb1df3
Aljon Gabriel <aljongabrielambasvaldez@gmail.com>
2025-12-02 02:59:07 +0800

    Adding more Sections

--- frontend/src/data/sections.js
+++ frontend/src/data/sections.js
@@ -3,62 +3,240 @@
 const sections = {
   1: [
     // A Block
-    "A101",
-    "A102",
-    "A103",
-    "A104",
-    "A105",
-    "A106",
+    'A101',
+    'A102',
+    'A103',
+    'A104',
+    'A105',
+    'A106',
     // AI Block
-    "AI101",
-    "AI102",
-    "AI103",
-    "AI104",
-    "AI105",
-    "AI106",
+    'AI101',
+    'AI102',
+    'AI103',
+    'AI104',
+    'AI105',
+    'AI106',
+    //ICSLIS SECTIONS
+    //Associate of Computer Technology
+    'AC-101',
+    'AC-102',
+    'AC-103',
+    'AC-104',
+    //Computer Science
+    'CS-101',
+    'CS-102',
+    'CS-103',
+    'CS-104',
+    //Information Systems
+    'IS-101',
+    'IS-102',
+    'IS-103',
+    //BS Library and Information Science
+    'L-101',
+
+    //IEAS SECTIONS
+    //Bachelor of Science in Mathematics
+    'M-101',
+    //Bachelor of Special Need Education
+    'NE-101',
+    //Bachelor of Science in Psychology
+    'P-101',
+    'P-102',
+    'P-103',
+    'P-104',
+    //Bachelor of Physical Education
+    'PE-101',
+    'PE-102',
+    'PE-103',
+    'PE-104',
+    'PE-105',
+    //Bachelor of Technical-Vocational Teacher Education
+    'TVTE-101',
+    'TVTE-102',
+
+    //Bachelor of Arts in English Language Studies
+    'AE-101',
+    'AE-102',
+    'AE-103',
   ],
   2: [
     // A Block
-    "A201",
-    "A202",
-    "A203",
-    "A204",
-    "A205",
-    "A206",
+    'A201',
+    'A202',
+    'A203',
+    'A204',
+    'A205',
+    'A206',
     // AI Block
-    "AI201",
-    "AI202",
-    "AI203",
-    "AI204",
-    "AI205",
-    "AI206",
+    'AI201',
+    'AI202',
+    'AI203',
+    'AI204',
+    'AI205',
+    'AI206',
+
+    //ICSLIS SECTIONS
+    //Associate of Computer Technology
+    'AC-201',
+    'AC-202',
+    'AC-203',
+    'AC-204',
+    //Computer Science
+    'CS-201',
+    'CS-202',
+    'CS-203',
+    'CS-204',
+    'CS-205',
+
+    //Information Systems
+    'IS-201',
+    'IS-202',
+    'IS-203',
+
+    //BS Library and Information Science
+    'L-201',
+
+    //IEAS SECTIONS
+    //Bachelor of Science in Mathematics
+    'M-201',
+    //Bachelor of Special Need Education
+    'NE-201',
+    //Bachelor of Science in Psychology
+    'P-201',
+    'P-202',
+    'P-203',
+    'P-204',
+    'P-205',
+    //Bachelor of Physical Education
+    'PE-201',
+    'PE-202',
+
+    //Bachelor of Technical-Vocational Teacher Education
+    'TVTE-201',
+    'TVTE-202',
+    'TVTE-203',
+
+    //Bachelor of Arts in English Language Studies
+    'AE-201',
+    'AE-202',
   ],
   3: [
     // A Block
-    "A301",
-    "A302",
-    "A303",
-    "A304",
-    "A305",
-    "A306",
+    'A301',
+    'A302',
+    'A303',
+    'A304',
+    'A305',
+    'A306',
     // AI Block
-    "AI301",
-    "AI302",
-    "AI303",
-    "AI304",
-    "AI305",
-    "AI306",
+    'AI301',
+    'AI302',
+    'AI303',
+    'AI304',
+    'AI305',
+    'AI306',
+
+    //ICSLIS SECTIONS
+    //Associate of Computer Technology
+
+    //Computer Science
+    'CS-301',
+    'CS-302',
+    'CS-303',
+    'CS-304',
+    'CS-305',
+    'CS-306',
+    'CS-307',
+    //Information Systems
+    'IS-301',
+    'IS-302',
+    'IS-303',
+    'IS-304',
+
+    //BS Library and Information Science
+    'L-301',
+
+    //IEAS SECTIONS
+    //Bachelor of Science in Mathematics
+    'M-301',
+
+    //Bachelor of Special Need Education
+    'NE-301',
+
+    //Bachelor of Science in Psychology
+    'P-301',
+    'P-302',
+    'P-303',
+    'P-304',
+    'P-305',
+
+    //Bachelor of Physical Education
+    'PE-301',
+    'PE-302',
+
+    //Bachelor of Technical-Vocational Teacher Education
+    'TVTE-301',
+    'TVTE-302',
+    'TVTE-303',
+
+    //Bachelor of Arts in English Language Studies
+    'AE-301',
+    'AE-302',
   ],
   4: [
     // A Block
-    "A401",
-    "A402",
-    "A403",
+    'A401',
+    'A402',
+    'A403',
     // AI Block
-    "AI401",
-    "AI402",
-    "AI403",
-    "AI404",
+    'AI401',
+    'AI402',
+    'AI403',
+    'AI404',
+
+    //ICSLIS SECTIONS
+    //Computer Science
+    'CS-401',
+    'CS-402',
+    'CS-403',
+    'CS-404',
+    'CS-405',
+    'CS-406',
+    'CS-407',
+    'CS-408',
+
+    //Information Systems
+    'IS-401',
+    'IS-402',
+    'IS-403',
+    'IS-404',
+
+    //BS Library and Information Science
+    'L-401',
+
+    //IEAS SECTIONS
+    //Bachelor of Science in Mathematics
+    'M-401',
+    //Bachelor of Special Need Education
+    'NE-401',
+    //Bachelor of Science in Psychology
+    'P-401',
+    'P-402',
+    'P-403',
+    'P-404',
+    'P-405',
+    //Bachelor of Physical Education
+    'PE-401',
+    'PE-402',
+    'PE-403',
+    //Bachelor of Technical-Vocational Teacher Education
+    'TVTE-401',
+    'TVTE-402',
+    'TVTE-403',
+
+    //Bachelor of Arts in English Language Studies
+    'AE-401',
+    'AE-402',
   ],
 };
 
